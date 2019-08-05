97afad53c6d62eaa32043a8b7789426fbba18ab3
seldii <seldaguzel@gmail.com>
2019-08-05 20:47:48 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ app.use("/google", require("./routes/google"));
 
 app.use("/auth", require("./routes/auth"));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //DB Config
 const db = config.get("mongoURI");
 
