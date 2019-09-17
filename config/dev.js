eff951af23810c01162ac1b7496f2ec7ec97bfb9
seldii <seldaguzel@gmail.com>
2019-09-17 22:29:01 +0200

    removed config/devjs

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Seldii:Ubiqum2019@cluster0-n74ce.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  jwtSecret: "Berlin_Ubiqum_Coder_Secret_123",
-  clientID:
-    "389070354277-444pehrr8l1gh7ioki1cvaia3rvbdkiu.apps.googleusercontent.com",
-  clientSecret: "qd7fnSh-dOIfvms446kXlTez"
-};
